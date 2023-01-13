1e24508eed1ce9dae977f86ead97b1ca95ddcfe7
lewah <ingridlewah@gmail.com>
2023-01-13 15:55:28 +0300

    Start

--- Strings/Strings/Program.cs
+++ Strings/Strings/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+}

cea01574dc1b203a6d007e2ef21365ee82bfe310
lewah <ingridlewah@gmail.com>
2023-01-13 16:45:28 +0300

    Working with Replacement code and strings

--- Strings/Strings/Program.cs
+++ Strings/Strings/Program.cs
@@ -14,16 +14,24 @@ namespace WorkingWithStrings
         {
             // manipulating strings
 
-            // back slash - \
+            //----- back slash - \
             //string myString = "My \"so called\" life";
 
-            // Add new line
+            //----- Add new line
             //string myString = "My first line \n My second line";
 
-            // using Back slash as string - \\
+            //----- using Back slash as string - \\
             //string myString = "Go to your c:\\ drive";
-            string myString = @"Go to your c:\ drive";
+            //string myString = @"Go to your c:\ drive";
 
+            //----- Replacement code  and currency(C) and large number(N) and percentage(P) and Phone Number(###) 
+            //string myString = String.Format("{0} = {1}", "First", "second");
+            //string myString = String.Format("{0:C}", 123.45);
+            //string myString = String.Format("{0:N}", 1234567890);
+            //string myString = String.Format("Percentage : {0:P}", .199);
+            string myString = String.Format("Phone Number : {0.(###) ###-####}", 254767676767);
+
+            //
             Console.WriteLine(myString);
             Console.ReadLine();
         }
